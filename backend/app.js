a34f3d8c6e07250d5783e3d485777b0448455371
arnob <farhanasfar21@gmail.com>
2025-07-24 02:04:26 +0600

    Add appointment route

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 import express from "express"
-
+import cors from "cors"
+import { appointmentRouter } from "./routes/appointment.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
+app.use("api/v1/booking", appointmentRouter);
 
 export{
     app,
